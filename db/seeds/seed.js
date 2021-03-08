2a963def9911aaf69127cd0edd347cd5febe1dc0
alice <alice.m.bailey@hotmail.co.uk>
2021-03-08 16:54:49 +0000

    seed

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -3,8 +3,32 @@ const {
   articleData,
   commentData,
   userData,
-} = require('../data/index.js');
+} = require("../data/index.js");
+
+const addVotes = require("../utils/data-manipulation");
 
 exports.seed = function (knex) {
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      return knex
+        .insert(userData)
+        .into("users")
+        .returning("*")
+        .then((addedUsers) => {
+          return knex
+            .insert(topicData)
+            .into("topics")
+            .returning("*")
+            .then((topicData) => {
+              const formattedArticleData = addVotes(articleData);
+              return knex
+                .insert(formattedArticleData)
+                .into("articles")
+                .returning("*");
+            });
+        });
+    });
   // add seeding functionality here
 };
--- db/utils/data-manipulation.js
+++ db/utils/data-manipulation.js
@@ -1,1 +1,25 @@
 // extract any functions you are using to manipulate your data, into this file
+const addVotes = function (array) {
+  const newArray = [];
+
+  array.forEach((article) => {
+    let newArticle = { ...article };
+    if (newArticle.votes === undefined) {
+      newArticle.votes = 0;
+    }
+    newArray.push(newArticle);
+  });
+  return newArray;
+};
+
+module.exports = addVotes;
+
+const formatTime = function (unixTime) {
+  const milliSeconds = unixTime * 1000;
+  const dateObj = new Date(milliSeconds);
+  const humanFormat = dateObj.toLocaleString();
+  console.log(dateObj.toISOString());
+  // console.log(humanFormat);
+};
+
+formatTime(1575909015000);
